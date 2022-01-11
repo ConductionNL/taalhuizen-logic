fede66b591f83ab281433d6b1c3a0446be6bda09
Wilco Louwerse <wilco@conduction.nl>
2022-01-11 12:25:24 +0100

    first draft logic that checks for existing user with username/email

--- api/src/Service/MailService.php
+++ api/src/Service/MailService.php
@@ -5,20 +5,23 @@ namespace App\Service;
 
 
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Environment;
 
 class MailService
 {
     private CommonGroundService $commonGroundService;
     private Environment $twig;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(CommonGroundService $commonGroundService, Environment $twig)
+    public function __construct(CommonGroundService $commonGroundService, Environment $twig, ParameterBagInterface $parameterBag)
     {
         $this->commonGroundService = $commonGroundService;
         $this->twig = $twig;
+        $this->parameterBag = $parameterBag;
     }
 
-    public function sendWelcomeMail(array $user, string $subject, string $frontend): bool
+    public function sendWelcomeMail(array $user, string $subject): bool
     {
         $response = $this->commonGroundService->getResourceList(['component' => 'uc', 'type' => 'users', 'id' => "{$user['id']}/token"], ['type' => 'SET_PASSWORD']);
         $person = $this->commonGroundService->isResource($user['person']);
@@ -28,7 +31,7 @@ class MailService
             'fullname'              => $person['name'] ?? $user['username'],
             'base64_encoded_email'  => base64_encode($user['username']),
             'base64_encoded_token'  => base64_encode($response['token']),
-            'app_base_url'          => rtrim($frontend, '/'),
+            'app_base_url'          => rtrim($this->parameterBag->get('frontendLocation'), '/'),
             'subject'               => $subject,
         ];
 
@@ -64,6 +67,34 @@ class MailService
         $message = $this->commonGroundService->createResource(
             [
                 'reciever' => $shareStudent['email'],
+                'sender'   => 'taalhuizen@biscutrecht.nl',
+                'content'  => $content,
+                'type'     => 'email',
+                'status'   => 'queued',
+                'service'  => '/services/' . $service['id'],
+                'subject'  => $subject,
+            ],
+            ['component' => 'bs', 'type' => 'messages']
+        );
+
+        return true;
+    }
+
+    public function sendEmployeeExistsMail(array $user, string $subject): bool
+    {
+        $person = $this->commonGroundService->isResource($user['person']);
+        $service = $this->commonGroundService->getResourceList(['component' => 'bs', 'type' => 'services'])['hydra:member'][0];
+        $parameters = [
+            'fullname' => $person['name'] ?? $user['username'],
+            'username' => $user['username'],
+            'subject'   => $subject,
+        ];
+
+        $content = $this->twig->render('share-student-e-mail.html.twig', $parameters); // TODO: new email template
+
+        $message = $this->commonGroundService->createResource(
+            [
+                'reciever' => $user['username'],
                 'sender'   => 'taalhuizen@biscutrecht.nl',
                 'content'  => $content,
                 'type'     => 'email',
