3abb00e48a833a249b11d143132aa57d81d143f9
Wilco Louwerse <wilco@conduction.nl>
2022-04-08 15:38:32 +0200

    update docblocks & start function for cron-job participation status

--- api/src/Service/ParticipationService.php
+++ api/src/Service/ParticipationService.php
@@ -19,7 +19,7 @@ class ParticipationService
     }
 
     /**
-     * Returns a participation body with the correct updated status, depending on the participation type (providerOption)
+     * Updates and returns a participation with the correct status, depending on the participation type (providerOption).
      *
      * @param array $participation
      * @return array
@@ -27,9 +27,11 @@ class ParticipationService
     public function setStatus(array $participation): array
     {
         if ($participation['providerOption'] == 'OTHER' && $participation['status'] == "REFERRED") {
-            $participationUpdate['status'] = "ACTIVE";
-            $participationUpdate['learningNeed'] = $participation['learningNeed']['id'];
-            $participationUpdate['providerOption'] = $participation['providerOption'];
+            $participationUpdate = [
+                'status'            => "ACTIVE",
+                'learningNeed'      => $participation['learningNeed']['id'],
+                'providerOption'    => $participation['providerOption']
+            ];
             return $this->commonGroundService->updateResource($participationUpdate,
                 ['component' => 'gateway', 'type' => 'participations', 'id' => $participation['id']]
             );
@@ -38,6 +40,12 @@ class ParticipationService
         return $participation;
     }
 
+    /**
+     * Checks if the student.referred datetime of the given participation.learningNeed.student needs to be set, and if so does this and returns the update student.
+     *
+     * @param array $participation
+     * @return array
+     */
     public function checkStudentReferred(array $participation): array
     {
         $totalStudentParticipations = $this->commonGroundService->getResourceList(
@@ -58,5 +66,33 @@ class ParticipationService
         }
 
         return [];
+    }
+
+    /**
+     * @TODO
+     *
+     * @return array
+     */
+    public function updateCompletedParticipations(): array
+    {
+        $result = [];
+        $participations = $this->commonGroundService->getResourceList(
+            ['component' => 'gateway', 'type' => 'participations'],
+            ['status' => 'ACTIVE', 'fields[]' => ['status', 'learningNeed', 'providerOption']], // todo: add: end[from] 0:00 today end[till] 0:00 tomorrow
+            false
+        )['results'];
+
+        foreach ($participations as $participation) {
+            $participationUpdate = [
+                'status'            => "COMPLETED",
+                'learningNeed'      => $participation['learningNeed']['id'],
+                'providerOption'    => $participation['providerOption']
+            ];
+            $result[] = $this->commonGroundService->updateResource($participationUpdate,
+                ['component' => 'gateway', 'type' => 'participations', 'id' => $participation['id']]
+            )['@uri'];
+        }
+
+        return $result;
     }
 }
