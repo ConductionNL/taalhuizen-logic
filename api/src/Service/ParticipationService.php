1cf08e32596322c4660d995ed5bf599df5ae29dc
Wilco Louwerse <wilco@conduction.nl>
2022-04-12 16:33:43 +0200

    Added command app:gateway:values that updates values with DateTimeValue

--- api/src/Service/ParticipationService.php
+++ api/src/Service/ParticipationService.php
@@ -5,6 +5,10 @@ namespace App\Service;
 
 
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use DateTime;
+use Exception;
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ParticipationService
@@ -50,7 +54,7 @@ class ParticipationService
     {
         $totalStudentParticipations = $this->commonGroundService->getResourceList(
             ['component' => 'gateway', 'type' => 'participations'],
-            ['learningNeed.student.id' => $participation['learningNeed']['student']['id'], 'fields[]' => null],
+            ['learningNeed.student.id' => $participation['learningNeed']['student']['id'], 'fields' => null],
             false
         )['total'];
 
@@ -76,23 +80,69 @@ class ParticipationService
     public function updateCompletedParticipations(): array
     {
         $result = [];
+        $today = new DateTime();
+        $today = $today->format('Y-m-d');
         $participations = $this->commonGroundService->getResourceList(
             ['component' => 'gateway', 'type' => 'participations'],
-            ['status' => 'ACTIVE', 'fields[]' => ['status', 'learningNeed', 'providerOption']], // todo: add: end[from] 0:00 today end[till] 0:00 tomorrow
+            [
+                'end[before]' => $today,
+                'status' => 'ACTIVE',
+                'fields' => ['status', 'learningNeed.id', 'providerOption']
+            ],
             false
         )['results'];
+//        var_dump(count($participations));
 
-        foreach ($participations as $participation) {
-            $participationUpdate = [
-                'status'            => "COMPLETED",
-                'learningNeed'      => $participation['learningNeed']['id'],
-                'providerOption'    => $participation['providerOption']
-            ];
-            $result[] = $this->commonGroundService->updateResource($participationUpdate,
-                ['component' => 'gateway', 'type' => 'participations', 'id' => $participation['id']]
-            )['@uri'];
+//        foreach ($participations as $participation) {
+//            $participationUpdate = [
+//                'status'            => "COMPLETED",
+//                'learningNeed'      => $participation['learningNeed']['id'],
+//                'providerOption'    => $participation['providerOption']
+//            ];
+//            $result[] = $this->commonGroundService->updateResource($participationUpdate,
+//                ['component' => 'gateway', 'type' => 'participations', 'id' => $participation['id']]
+//            )['@uri'];
+//        }
+
+        return $participations;
+    }
+
+    public function updateGatewayDateTimeValues(SymfonyStyle $io, $page = 1, int $errorCount = 0, bool $first = true)
+    {
+        $values = $this->commonGroundService->getResourceList(
+            ['component' => 'gatewayAdmin', 'type' => 'values'],
+            ['page' => $page, 'exists[dateTimeValue]' => 'true', 'exists[stringValue]' => 'false'],
+            false
+        );
+        if ($first) {
+            $io->progressStart($values['hydra:totalItems']);
         }
 
-        return $result;
+        foreach ($values['hydra:member'] as $value) {
+            $io->section("Checking value {$value['id']}");
+            try {
+                $date = new DateTime($value['dateTimeValue']);
+                $date = $date->format('Y-m-d H:i:s');
+                $updateValue = ['stringValue' => $date];
+                $this->commonGroundService->updateResource($updateValue,
+                    ['component' => 'gatewayAdmin', 'type' => 'values', 'id' => $value['id']]
+                );
+                $io->text('Value now has stringValue: '.$date);
+            } catch (RequestException | Exception $exception) {
+                $io->error($exception->getMessage());
+                $errorCount++;
+            }
+            $io->progressAdvance();
+        }
+
+        if (isset($values['hydra:view']['hydra:next'])) {
+            return $this->updateGatewayDateTimeValues($io, $page + 1, $errorCount, false);
+        }
+
+        $io->progressFinish();
+        if ($values['hydra:totalItems'] == 0) {
+            return 0;
+        }
+        return round($errorCount/$values['hydra:totalItems']*100) == 0 && $errorCount > 0 ? 1 : round($errorCount/$values['hydra:totalItems']*100);
     }
 }
